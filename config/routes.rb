f42968f03a48c9b83a415fb647cac626dc8aa763
root <root@otomo>
2025-01-21 22:55:50 +0900

    fix:rubocopの確認と修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root 'static_pages#start'
+  root "static_pages#start"
 end
