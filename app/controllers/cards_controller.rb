f71d1532bc2b34c881c456a937fbee919d410ffc
root <root@otomo>
2025-01-28 00:00:09 +0900

    fix:profileのdbを一緒に取り出すよう変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,6 @@ class CardsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @cards = Card.includes(:user)
+    @cards = Card.includes(:user,:profile)
   end
 end
